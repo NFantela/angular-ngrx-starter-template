6adcb44ee5288c10d46bb6e9c70af45fa3b5997b
Nikša Fantela <niksa.simple.stack@gmail.com>
2020-03-10 09:50:47 +0100

    Initial Setup

---
+++ projects/demo-app-template/src/app/core/core.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+@NgModule({
+    imports: [
+        BrowserModule,
+        BrowserAnimationsModule
+    ],
+    declarations: [],
+    providers: []
+})
+export class CoreModule {}
