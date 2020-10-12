f35365da4372ca5c7e2708b64b8e20d9c9b71638
Nikša Fantela <niksa@robotiq.ai>
2020-10-12 10:22:11 +0200

    Extended memoization decorator

--- projects/demo-app-template/src/app/shared/decorators/pure-memoization.decorator.ts
+++ projects/demo-app-template/src/app/shared/decorators/pure-memoization.decorator.ts
@@ -6,45 +6,68 @@
   return items.filter(item => 
     item.toLowerCase().includes(value.toLowerCase())
   );
+  or lazy computation for getter : only once and never again e.g. credit card validity 
+  @PureMemoization
+    get valid(): boolean {
+    return validate(this.cardNumber);
+  }
 }
  */
 
-export function PureMemoization<T>(
-    _target: Object,
-    propertyKey: string,
-    { enumerable, value }: TypedPropertyDescriptor<T>
-  ): TypedPropertyDescriptor<T> {
-    if (typeof value !== "function") {
-      throw new Error("Pure can only be used with functions or getters");
-    }
-    // passed fn
-    const original = value;
-  
+function PureMemoization<T>(
+  _: object,
+  propertyKey: string,
+  { get, enumerable, value }: TypedPropertyDescriptor<T>
+): TypedPropertyDescriptor<T> {
+  // if we have a getter fn for property
+  // lazy computation only when called 
+  if (get) {
     return {
       enumerable,
       get(): T {
-        let previousArgs: ReadonlyArray<any> = [];
-        let previousResult: any;
-  
-        const patched = (...args: Array<any>) => {
-          if (
-            previousArgs.length === args.length &&
-            args.every((arg, index) => arg === previousArgs[index])
-          ) {
-            return previousResult;
-          }
-  
-          previousArgs = args;
-          previousResult = original(...args);
-  
-          return previousResult;
-        };
-  
+        const currentValue = get.call(this);
+
         Object.defineProperty(this, propertyKey, {
-          value: patched
+          enumerable,
+          value: currentValue
         });
-  
-        return patched as any;
+
+        return currentValue;
       }
     };
   }
+
+  if (typeof value !== "function") {
+    throw new Error("Pure can only be used with functions or getters");
+  }
+  // if we are decorating a function
+  const original = value;
+
+  return {
+    enumerable,
+    get(): T {
+      let previousArgs: ReadonlyArray<unknown> = [];
+      let previousResult: any;
+
+      const patched = (...args: Array<unknown>) => {
+        if (
+          previousArgs.length === args.length &&
+          args.every((arg, index) => arg === previousArgs[index])
+        ) {
+          return previousResult;
+        }
+
+        previousArgs = args;
+        previousResult = original(...args);
+
+        return previousResult;
+      };
+
+      Object.defineProperty(this, propertyKey, {
+        value: patched
+      });
+
+      return patched as any;
+    }
+  };
+}
